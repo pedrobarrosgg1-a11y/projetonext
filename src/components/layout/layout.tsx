0391a235526e3a7a81bebb1d6d9891506e9b3288
pedrobarrosgg1-a11y <pedro.barros.gg1@gmail.com>
2026-05-23 11:50:55 -0300

    Style guide created. 🎨

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,18 +1,21 @@
-import { Header } from "../header"
-import { Footer } from "../footer/footer.tsx"
+import { Inter } from 'next/font/google';
+import { Footer } from '@/src/components/footer/footer.tsx';
+import { Header } from '../header';
 
 type LayoutProps = {
-    children: React.ReactNode;
-}
- 
-export const Layout = ({children}: LayoutProps) => {
-    return (
-        <div className="relative flex min-h-screen flex-col dark">
-            <Header />
-            <main className="flex-1 flex flex-col mb-12">
-            {children}
-            </main>
-            <Footer />
-        </div>
-    )
-}
+  children: React.ReactNode;
+};
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const Layout = ({ children }: LayoutProps) => {
+  return (
+    <div
+      className={`${inter.className} relative flex min-h-screen flex-col bg-gray-700`}
+    >
+      <Header />
+      <main className="flex-1 flex flex-col mb-12">{children}</main>
+      <Footer />
+    </div>
+  );
+};
