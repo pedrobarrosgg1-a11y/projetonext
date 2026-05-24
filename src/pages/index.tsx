f2dfc78fefc3c1391ce0632881602083d636c1fd
pedrobarrosgg1-a11y <pedro.barros.gg1@gmail.com>
2026-05-24 15:49:02 -0300

    Support Section added.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
-import { FeatureSection } from '../components/feature-section';
-import { HeroSection } from '@/src/components/hero-section';
+import { FeatureSection } from "../components/feature-section";
+import { HeroSection } from "@/src/components/hero-section";
+import { SupportSection } from "../components/support-section";
 
 export default function Home() {
   return (
-   <>
+    <>
       <article className="flex flex-col">
-       <HeroSection />
-       <FeatureSection />
+        <HeroSection />
+        <FeatureSection />
+        <SupportSection />
       </article>
-   </>
+    </>
   );
 }
