2efff2d4af6f6b0d6ba666ef834ecef4bb837234
pedrobarrosgg1-a11y <pedro.barros.gg1@gmail.com>
2026-05-25 21:17:04 -0300

    Refactoring and improvements to the project.

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter, PT_Sans_Caption } from 'next/font/google';
-import { Footer } from '@/src/components/footer/footer.tsx';
-import { Header } from '../header';
+import { Footer } from './footer/footer.tsx';
+import { Header } from './header';
 
 type LayoutProps = {
   children: React.ReactNode;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,5 @@
-import { FeatureSection } from "../components/feature-section";
-import { HeroSection } from "@/src/components/hero-section";
-import { SupportSection } from "../components/support-section";
-import { CustomerStorySection as CustomerSection } from "@/src/components/customer-section";
-import { CallToAction } from "../components/call-to-action/call-to-action";
+import { LandingPage } from '@/src/templates/landing-page';
 
 export default function Home() {
-  return (
-    <>
-      <article className="flex flex-col">
-        <HeroSection />
-        <FeatureSection />
-        <SupportSection />
-        <CustomerSection />
-        <CallToAction />  
-      </article>
-    </>
-  );
+  return <LandingPage />;
 }
