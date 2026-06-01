398e91dc47892d90f55cc56b25ffd1cde558ac5b
pedrobarrosgg1-a11y <pedro.barros.gg1@gmail.com>
2026-06-01 18:45:07 -0300

    Research component completed.

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,7 +1,7 @@
-import { cn } from '@/src/lib/utils';
-import { SearchIcon } from 'lucide-react';
-import { useRouter } from 'next/router';
-import { useCallback } from 'react';
+import { cn } from "@/src/lib/utils";
+import { CircleX, SearchIcon } from "lucide-react";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 export const Search = () => {
   const router = useRouter();
@@ -14,7 +14,7 @@ export const Search = () => {
         router.push(`/blog?q=${encodeURIComponent(query)}`);
       }
     },
-    [query, router]
+    [query, router],
   );
 
   const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -25,21 +25,33 @@ export const Search = () => {
     });
   };
 
+  const resetSearch = useCallback(() => {
+    router.push("/blog", undefined, { shallow: true, scroll: false });
+  }, [router]);
+
   return (
-    <form onSubmit={handleSearch} className="relative group">
+    <form onSubmit={handleSearch} className="relative group w-full md:w-60">
       <SearchIcon
         className={cn(
-          'text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300',
-          query ? ' text-blue-300' : ''
+          "text-gray-300 absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 transition-colors duration-200 group-focus-within:text-blue-300",
+          query ? " text-blue-300" : "",
         )}
       />
 
       <input
         type="text"
+        value={query || ""}
         placeholder="Buscar"
         onChange={handleQueryChange}
-        className="h-10 w-72 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
+        className="w-full h-10 md:w-60 bg-transparent border border-gray-400 pl-9 text-gray-100 rounded-md text-body-sm outline-none transition-all duration-200 focus-within:border-blue-300 focus-within:ring-1 focus-within:ring-blue-300 placeholder:text-gray-300 placeholder:text-body-sm"
       />
+
+      {query && (
+        <CircleX
+          className="absolute h-4 w-4 top-1/2 -translate-y-1/2 right-3 text-gray-300"
+          onClick={resetSearch}
+        />
+      )}
     </form>
   );
 };
