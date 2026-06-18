7586803e415ffc26b251abcb194d8b64e1580ee4
pedrobarrosgg1-a11y <pedro.barros.gg1@gmail.com>
2026-06-18 18:23:22 -0300

    Added revalidation and static parameter generation for blog post pages

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -8,6 +8,14 @@ type BlogPostPage = {
   }>;
 };
 
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  return getAllPosts().map((post) => ({
+    slug: post.slug,
+  }));
+}
+
 export default async function BlogPostPage({params}: BlogPostPage) {
     const { slug } = await params;
     const post = getAllPosts().find((post) => post.slug === slug);
